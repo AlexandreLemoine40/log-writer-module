530b0dbfeb9f6ca2c7886cb5ad7f6145b789a7f9
alexandrelemoine40 <lemoine.alexandre40@gmail.com>
2019-06-03 16:34:13 +0200

    Correction of an issue that the month returned was decremented of 1

--- logModule.js
+++ logModule.js
@@ -1,22 +1,22 @@
 let fs = require('fs')
 
-// Module d'écriture dans le fichier de logs
+// Write a log file into a folder named logs at the root of the project, if exists, add a new line.
 module.exports = (string) => {
-  // Date actuelle
+  // Actual date
   let dateLogs = new Date()
 
-  let months = (dateLogs.getMonth() < 10 ? '0' + dateLogs.getMonth() : dateLogs.getMonth())
+  let month = dateLogs.getMonth() + 1
+  let months = (month < 10 ? '0' + month : month)
   let days = (dateLogs.getDate() < 10 ? '0' + dateLogs.getDate() : dateLogs.getDate())
-
 
   let hours = (dateLogs.getHours() < 10 ? '0' + dateLogs.getHours() : dateLogs.getHours())
   let minutes = (dateLogs.getMinutes() < 10 ? '0' + dateLogs.getMinutes() : dateLogs.getMinutes())
   let seconds = (dateLogs.getSeconds() < 10 ? '0' + dateLogs.getSeconds() : dateLogs.getSeconds())
 
-  // Nom du fichier de logs
+  // Text file name
   let logsName = dateLogs.getFullYear() + '_' + months + '_' + days + '_logs.txt'
 
-  // Ouvre le fichier de log du jour si il existe, sinon le créer
+  // Open and write or just write
   fs.appendFile('logs/' + logsName, '[' + hours + ':' + minutes + ':' + seconds + ']' + string + '\n', (err) => {
     if (err) throw err
   })
